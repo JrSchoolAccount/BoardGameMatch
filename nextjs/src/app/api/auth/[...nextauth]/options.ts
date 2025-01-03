a762c8edd86978640ff000d1e55a91e3500a5485
JrSchoolAccount <jesperx2000@gmail.com>
2025-01-03 10:14:26 +0100

    Added NextAuth with github provider

---
+++ nextjs/src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,20 @@
+import type {AuthOptions} from "next-auth";
+import GitHubProvider from "next-auth/providers/GitHub";
+
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET;
+
+if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
+    throw new Error('GitHub Client ID and Secret not set in environment variables!')
+}
+
+export const options: AuthOptions = {
+    providers: [
+        GitHubProvider({
+            clientId: GITHUB_CLIENT_ID,
+            clientSecret: GITHUB_CLIENT_SECRET,
+        }),
+    ],
+    secret: NEXTAUTH_SECRET,
+};
